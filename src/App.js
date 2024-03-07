1ecce977cbace2d784a45d9cbce45bd1d5129f13
Jaffy <127703538+jaffymaglinte@users.noreply.github.com>
2024-03-07 17:51:56 +0800

    feat: added intersection observer

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
 // import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./components/Common/Sidebar/Sidebar";
+import Header from "./components/Common/Header/Header";
+import Error404 from "./components/Error/404";
+
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";  
-import './App.css'
-import Sidebar from './components/Common/Sidebar/Sidebar'
-import Header from './components/Common/Header/Header'
-import Error404 from './components/Error/404'  
-
-import { Home, 
-  Single, TVShowSingle,
-  SearchDetail, 
+  Home,
+  Single,
+  TVShowSingle,
+  SearchDetail,
   Movies,
-  TVShows
-} from './pages/index' 
+  TVShows,
+} from "./pages/index";
 
-function App() { 
-  
+function App() {
   const handleClickApp = (e) => {
-    e.preventDefault() 
-    let sidebar = document.getElementById('sidebar')
-    let overlay = document.getElementById('overlay')
-    
+    e.preventDefault();
+    let sidebar = document.getElementById("sidebar");
+    let overlay = document.getElementById("overlay");
 
-    sidebar.classList.remove('open')
-    sidebar.classList.add('closed')
+    sidebar.classList.remove("open");
+    sidebar.classList.add("closed");
 
-    overlay.classList.remove('open')
-    overlay.classList.add('closed') 
-  }
-  
+    overlay.classList.remove("open");
+    overlay.classList.add("closed");
+  };
+
   return (
     <Router>
-      <div className="App"> 
+      <div className="App">
         <div id="overlay" className="closed" onClick={handleClickApp}></div>
         <Sidebar></Sidebar>
         <div id="main-container">
-          <Header></Header>
-          <Switch> 
-            <Route exact path="/" component={Home}/>
-            <Route path="/movie/:id" component={Single}/>
-            <Route path="/search" component={SearchDetail}/>
-            <Route path="/movies" component={Movies}/>
-            <Route path="/tv-shows" component={TVShows}/>
-            <Route path="/tv/:id" component={TVShowSingle}/>  
-            <Route path="*" component={Error404}/>
-          </Switch> 
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/movie/:id" component={Single} />
+            <Route path="/search" component={SearchDetail} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/tv-shows" component={TVShows} />
+            <Route path="/tv/:id" component={TVShowSingle} />
+            <Route path="*" component={Error404} />
+          </Switch>
         </div>
       </div>
     </Router>
-  
-  )
+  );
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,95 +1,115 @@
-import {
-    Link
-} from "react-router-dom";
-import React, { useState, useEffect } from "react"
-import "./Home.css"
-import Config from "./../../api/config" 
-const axios = require('axios');
- 
+import { Link } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import "./Home.css";
+import Config from "./../../api/config";
+import { useInView } from "react-intersection-observer";
+
+const axios = require("axios");
+
 const Home = () => {
-    const [movieList, setMovieList] = useState([])
-    const [counter, setCounter] = useState(1)
+  const [movieList, setMovieList] = useState([]);
+  const [counter, setCounter] = useState(1);
+  const { ref, inView } = useInView();
 
-    // Similar to componentDidMount and componentDidUpdate:
-    useEffect(() => {
-        // Update the document title using the browser API
-        const getMovieList = async () => {
-            try {
-                const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${Config().API}&language=en-US&page=${counter}`)
-                setMovieList([...movieList, ...response.data.results])
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        getMovieList()
-    }, [counter]);
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/now_playing?api_key=${
+            Config().API
+          }&language=en-US&page=${counter}`
+        );
+        setMovieList((prev) => [...prev, ...response.data.results]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    window.scrollTo(0, document.body.scrollHeight);
-    
-    
-    const handleLoadMore = (e) => {
-        e.preventDefault()
-        setCounter(counter + 1)
-        console.log(counter)
+    getMovieList();
+  }, [counter]);
+
+  const handleFetch = useCallback(async () => {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${
+        Config().API
+      }&language=en-US&page=${counter}`
+    );
+    setMovieList((prev) => [...prev, ...response.data.results]);
+    setCounter((prev) => prev + 1);
+
+    console.log("asas");
+  }, [counter]);
+
+  useEffect(() => {
+    if (inView) {
+      handleFetch();
     }
-      
+  }, [inView, handleFetch]);
 
-    return (
-        <div id="homepage" className="container">
-            <div className="home">Home</div>
-            <section>
-                
-                <div className="main-blogs">
-                    <div className="main-blog anim">
-                        <div className="main-blog__title">
-                            SELECT person FROM world WHERE someone LIKE '%<i className="text-danger">you</i>%';
-                            <h6 className="recommend">Recommended browser: <a href="https://brave.com/" target="_blank" rel="noreferrer">Brave</a></h6>
-                        </div>
+  return (
+    <div id="homepage" className="container">
+      <div className="home">Home</div>
+      <section>
+        <div className="main-blogs">
+          <div className="main-blog anim">
+            <div className="main-blog__title">
+              SELECT person FROM world WHERE someone LIKE '%
+              <i className="text-danger">you</i>%';
+              <h6 className="recommend">
+                Recommended browser:{" "}
+                <a href="https://brave.com/" target="_blank" rel="noreferrer">
+                  Brave
+                </a>
+              </h6>
+            </div>
 
-                        <div className="main-blog__author">
-                            <div className="author-detail">
-                                <div className="author-name">Support Me.
-                                </div>
-                                <div className="author-info donate">
-                                    <a href="tel:09673643071">Gcash</a>
-                                    <span className="seperate"></span>
-                                    {/* <a href="https://www.paypal.me/JaffyMaglinte" target="_blank">Paypal</a> */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
+            <div className="main-blog__author">
+              <div className="author-detail">
+                <div className="author-name">Support Me.</div>
+                <div className="author-info donate">
+                  <a href="tel:09673643071">Gcash</a>
+                  <span className="seperate"></span>
+                  {/* <a href="https://www.paypal.me/JaffyMaglinte" target="_blank">Paypal</a> */}
                 </div>
-                {/* <Banner /> */}
-                <h2>Now playing movies</h2> 
-                <div className="poster-grid">
-                    {
-                        movieList.map((res) => {
-                            // console.log(res)
-                            const date = new Date(res.release_date)
-                            const poster = `https://image.tmdb.org/t/p/original${ res.poster_path }`
-                            const movieLink = `movie/${res.id}`
-                            return (
-                                <div key={res.id}>
-                                    <Link to={movieLink} className="poster-card" >
-                                        <img className="poster img-with-fb no-js-1MJNcPZy46hIy2CmSqOeru0yr5C" src={poster} cached="true" loading="lazy" alt="Poster for Venom: Let There Be Carnage" />
-                                        <div className="overlay-text">
-                                            <div className="overlay-text-rating">HD</div>
-                                        </div>
-                                        <p className="title">{res.original_title}</p>
-                                        <div className="meta">
-                                            {date.getFullYear()} <i className="dot"></i> {res.vote_average}
-                                            <i className="type">Movie</i>
-                                        </div>
-                                    </Link>
-                                </div>
-                            ) 
-                        })
-                    } 
-                </div>
-                <div className="more"><h6 onClick={handleLoadMore}>... Load More ...</h6></div>
-            </section>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+        {/* <Banner /> */}
+        <h2>Now playing movies</h2>
+        <div className="poster-grid">
+          {movieList.map((res, index) => {
+            const date = new Date(res.release_date);
+            const poster = `https://image.tmdb.org/t/p/original${res.poster_path}`;
+            const movieLink = `movie/${res.id}`;
+            return (
+              <div key={`id-${index}`}>
+                <Link to={movieLink} className="poster-card">
+                  <img
+                    className="poster img-with-fb no-js-1MJNcPZy46hIy2CmSqOeru0yr5C"
+                    src={poster}
+                    cached="true"
+                    loading="lazy"
+                    alt="Poster for Venom: Let There Be Carnage"
+                  />
+                  <div className="overlay-text">
+                    <div className="overlay-text-rating">HD</div>
+                  </div>
+                  <p className="title">{res.original_title}</p>
+                  <div className="meta">
+                    {date.getFullYear()} <i className="dot"></i>{" "}
+                    {res.vote_average}
+                    <i className="type">Movie</i>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <span ref={ref} />
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
